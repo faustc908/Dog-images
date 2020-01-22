dd8b5534012518687686a97b051f002202d98255
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-01-21 20:42:41 -0500

    Update script.js

--- random dog generator/script.js
+++ random dog generator/script.js
@@ -1,42 +1,32 @@
-'use strict';
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Random dog picture generator</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
 
-function inputListener() {
-  $('#dog-form').on('submit', function(event) {
-    $('.dog-image').empty();
-    event.preventDefault();
-    let dogInput = $('#dog-input').val();
-    getInput(dogInput);
-  });
-}
+    <header>
+    <span class="banner-text">Find your picture!</span>
+    </header>
 
-function getInput(dogInput) {
-    if (dogInput.length === 0) {
-      let userInput = 3;
-      pullDogImages(userInput);
-      } else if (dogInput > 50) {
-      alert('Number less then 50 needed');
-      } else if (dogInput < 1 ) {
-      alert('Number needed');
-      } else {
-      let userInput = dogInput;
-      pullDogImages(userInput);
-    }
-  }
-
-function pullDogImages(userInput) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${userInput}`)
-    .then(response => response.json())
-    .then(responseJson => showDogs(responseJson))
-    .catch(error => alert('There was an error'));
-}
-
-function showDogs(dogImages) {
-    for (var i = 0; i < dogImages.message.length; i++) {
-      $('.dog-image').append(`<img src="${dogImages.message[i]}">`);
-    };
-}
-
-$(function() {
-  inputListener();
-}
-);
+    <main>
+      <div>How many dogs would you like to see?</div>
+      <form id="dog-form">
+        <input id="dog-input" type="text" name="dog-number">
+        <input type="submit" name="submit">
+      </form>
+      <div>*Notes: Pick a number between 1 and 50. If you dont pick any number three random dogs will appear.</div>
+      <section>
+        <div class="dog-image"></div>
+      </section>
+    </main>
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+  </body>
+</html>
